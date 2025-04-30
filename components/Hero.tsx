28687b1576d00f0f43cfbf55957201dd7644fa35
Pratham Ranka <pranka0789@gmail.com>
2025-04-30 11:38:43 +0530

    refactor: comment out "Open for Work" badge in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,9 +34,9 @@ export const Hero = () => {
             className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200"
         >
             <div className="z-10 flex flex-col items-center">
-                <span className="mb-3 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
+                {/* <span className="mb-3 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
                     Open for Work
-                </span>
+                </span> */}
                 <h1 className="text-white/40 text-7xl font-black">Hi, I am</h1>
                 <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent md:text-7xl">
                     Pratham Ranka
