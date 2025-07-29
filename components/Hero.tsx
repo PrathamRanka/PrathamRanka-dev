01c90f62bc1e496a1a1c00e2954f2892707749fc
Pratham <pranka0789@gmail.com>
2025-07-29 13:54:06 +0530

    updated portfolio link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -79,7 +79,7 @@ export const Hero = () => {
                 {/* Center the "Download CV" button */}
                 <motion.div className="flex justify-center items-center">
                     <a
-                        href="https://drive.google.com/uc?export=download&id=1s273o3pVF9d4gg3vMWqlGGaEHx_ypB7k"
+                        href="https://drive.google.com/uc?export=download&id=1IBSUVxrarBctqbsrTR5jdDtBHIkUYxTp"
                         download="Pratham_Ranka_Resume.pdf"
                     >
                         <motion.button
